636ba7d99a5244659dc6083cc1c782024eae0463
ジェヒョン <h94051987@gmail.com>
2024-03-04 20:58:02 +0900

    it's been a long time

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -7,8 +7,9 @@ use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
-    //
+    // 로그인 페이지를 보여주는 메서드
     public function showLoginForm() {
+        // 'auth.login'뷰를 반환하여 로그인 페이지를 표시한다.
         return view('auth.login');
     }
 }
